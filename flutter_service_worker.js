94e0a49434efc97360ad09dbb650eab01cd8e9c8
zartyblartfast <zarty_blartfast@yahoo.com>
2025-02-04 00:53:52 +0000

    Deploy to GitHub Pages - 2025-02-04 00:53:52

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -52,7 +52,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "e523b49f7ac3d52fa2a74629c7ae09b1
 "favicon.png": "ef6f51d005dde33824532c166c395d0e",
 "favicon_old.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "c5a0bba4e67d3762727b148c404684a2",
+"flutter_bootstrap.js": "dd9ffd029bb6827e90792719b1031dca",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
